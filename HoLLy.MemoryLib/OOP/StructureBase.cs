da272f42c5e7b7b6f18b984aae674ecad990e2f7
HoLLy <dario.db@hotmail.com>
2018-04-04 23:19:01 +0200

    Make some StructureBase fields public

--- HoLLy.MemoryLib/OOP/StructureBase.cs
+++ HoLLy.MemoryLib/OOP/StructureBase.cs
@@ -4,18 +4,20 @@ namespace HoLLy.Memory.OOP
 {
     public class StructureBase
     {
-        public bool IsNull => _addr == IntPtr.Zero;
+        public MemoryAbstraction Memory => _mem;
+        public IntPtr Addr => _addr;
+        public bool IsNull => Addr == IntPtr.Zero;
         public bool Prefetched => _prefetchedBytes != null;
         protected virtual int PrefetchSize => -1;
 
-        protected readonly MemoryAbstraction Mem;
+        private readonly MemoryAbstraction _mem;
 
         private IntPtr _addr;
         private byte[] _prefetchedBytes;
 
         public StructureBase(MemoryAbstraction mem, IntPtr addr)
         {
-            Mem = mem;
+            _mem = mem;
             _addr = addr;
         }
 
@@ -26,22 +28,22 @@ namespace HoLLy.Memory.OOP
 #endif
             if (Prefetched || PrefetchSize < 0) return;
 
-            _prefetchedBytes = Mem.ReadBytes(_addr, PrefetchSize);
+            _prefetchedBytes = Memory.ReadBytes(Addr, PrefetchSize);
         }
 
-        private IntPtr Offset(int offset) => new IntPtr(_addr.ToInt64() + offset);
+        private IntPtr Offset(int offset) => new IntPtr(Addr.ToInt64() + offset);
 
         //prefetched
-        protected IntPtr Pointer(int offset) => Prefetched ? new IntPtr(BitConverter.ToInt32(_prefetchedBytes, offset)) : Mem.ReadIntPtr(Offset(offset));
-        protected int Int(int offset) => Prefetched ? BitConverter.ToInt32(_prefetchedBytes, offset) : Mem.ReadInt(Offset(offset));
-        protected float Float(int offset) => Prefetched ? BitConverter.ToSingle(_prefetchedBytes, offset) : Mem.ReadFloat(Offset(offset));
-        protected double Double(int offset) => Prefetched ? BitConverter.ToDouble(_prefetchedBytes, offset) : Mem.ReadDouble(Offset(offset));
+        protected IntPtr Pointer(int offset) => Prefetched ? new IntPtr(BitConverter.ToInt32(_prefetchedBytes, offset)) : Memory.ReadIntPtr(Offset(offset));
+        protected int Int(int offset) => Prefetched ? BitConverter.ToInt32(_prefetchedBytes, offset) : Memory.ReadInt(Offset(offset));
+        protected float Float(int offset) => Prefetched ? BitConverter.ToSingle(_prefetchedBytes, offset) : Memory.ReadFloat(Offset(offset));
+        protected double Double(int offset) => Prefetched ? BitConverter.ToDouble(_prefetchedBytes, offset) : Memory.ReadDouble(Offset(offset));
 
         //not prefetched
-        protected string String(int offset) => Mem.ReadDotNetString(Offset(offset));
-        //protected List<T> List<T>(int offset) where T : StructureBase => Mem.ReadDotNetList<T>(Offset(offset));
+        protected string String(int offset) => Memory.ReadDotNetString(Offset(offset));
+        //protected List<T> List<T>(int offset) where T : StructureBase => Memory.ReadDotNetList<T>(Offset(offset));
 
         //other stuff
-        public T To<T>() where T : StructureBase => (T)Activator.CreateInstance(typeof(T), Mem, _addr);
+        public T To<T>() where T : StructureBase => (T)Activator.CreateInstance(typeof(T), Memory, Addr);
     }
 }
